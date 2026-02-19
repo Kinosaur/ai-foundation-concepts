fd0a20488d14ab79d2a2366468e594561a62cb8c
Kino_saur <kaungkhantlin999@gmail.com>
2026-02-19 22:43:37 +0700

    Start doing project

---
+++ project/code/infer.py
@@ -0,0 +1,5 @@
+from ultralytics import YOLO
+
+model = YOLO(r"/Users/kaungkhantlin/Developer/2_2025/AI_Concepts/project/best.pt")
+results = model.predict(source=r"/Users/kaungkhantlin/Developer/2_2025/AI_Concepts/project/test/Image.jpeg", conf=0.25, save=True)
+print("Saved to:", results[0].save_dir)
