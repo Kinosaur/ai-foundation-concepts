14f3cdf5277a1ef2765568807c50803678d5bfbb
Kino_saur <kaungkhantlin999@gmail.com>
2026-02-22 20:54:49 +0700

    feat: Refactor YOLO training notebook, update inference script, and add result visualizations.

--- project/code/infer.py
+++ project/code/infer.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
 model = YOLO(r"/Users/kaungkhantlin/Developer/2_2025/AI_Concepts/project/best.pt")
-results = model.predict(source=r"/Users/kaungkhantlin/Developer/2_2025/AI_Concepts/project/test/Image.jpeg", conf=0.25, save=True)
+results = model.predict(source=r"/Users/kaungkhantlin/Developer/2_2025/AI_Concepts/project/test/test2.jpg", conf=0.25, save=True)
 print("Saved to:", results[0].save_dir)
